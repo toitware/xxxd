daf673d5ff32da5c26c5e3ed2bf6f5706907516c
Erik Corry <erik@toitware.com>
2023-09-12 16:56:11 +0200

    Print exact version of xxd

--- tests/get-xxd-version.sh
+++ tests/get-xxd-version.sh
@@ -3,6 +3,10 @@
 # Use of this source code is governed by a Zero-Clause BSD license that can
 # be found in the tests/TESTS_LICENSE file.
 
-# Note that `xxc -v` outputs the format: "xxd V1.10 27oct98 by Juergen Weigert"
+set -e
+
+xxd -v
+
+# Note that `xxc -v` outputs the format: "xxd 2021-07-07 ...
 # We only care about the year, so we use awk to extract that.
 xxd -v 2>&1 | awk '{print $2}' | awk -F- '{print $1}'
