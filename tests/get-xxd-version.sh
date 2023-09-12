f5cda65df2157dda31f00c26c8280344f4dbf42b
Erik Corry <erik@toitware.com>
2023-09-12 17:02:49 +0200

    Try to get xxd version on Windows

--- tests/get-xxd-version.sh
+++ tests/get-xxd-version.sh
@@ -1,4 +1,3 @@
-#!/bin/bash
 # Copyright (C) 2023 Toitware ApS.
 # Use of this source code is governed by a Zero-Clause BSD license that can
 # be found in the tests/TESTS_LICENSE file.
@@ -8,5 +7,5 @@ set -e
 xxd -v
 
 # Note that `xxc -v` outputs the format: "xxd 2021-07-07 ...
-# We only care about the year, so we use awk to extract that.
-xxd -v 2>&1 | awk '{print $2}' | awk -F- '{print $1}'
+# We only care about the date, so we use awk to extract that.
+xxd -v 2>&1 | awk '{print $2}'
